bc803a0748917b12cd54ac2ccc33998dd05991d0
KristianEmilWN <kewn21@student.aau.dk>
2024-05-20 10:12:47 +0200

    state test basics

--- Project/src/test/java/com/proj4/StateDeclTest.java
+++ Project/src/test/java/com/proj4/StateDeclTest.java
@@ -1,5 +1,37 @@
 package com.proj4;
 
-public class StateDeclTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.proj4.symbolTable.Scope;
+import com.proj4.symbolTable.ScopeObserver;
+import com.proj4.symbolTable.symbols.IntegerSymbol;
+import com.proj4.symbolTable.symbols.SymbolTableEntry;
+import com.proj4.symbolTable.symbols.TemplateSymbol;
+
+public class StateDeclTest extends TestingArgs{
+    private static HashMap<String, SymbolTableEntry> variableTable;
+
+    @BeforeAll
+    public static void setup() {
+        ScopeObserver scopeObserver = new ScopeObserver();
+        Scope.addObserver(scopeObserver);
+
+        DBL interpreter = new DBL();
+        interpreter.setDebugMode(debugMode);
+        interpreter.setVerbosity(verbose);
+        interpreter.interpret(getPath() + "statetest.dbl");
+
+        variableTable = scopeObserver.getCurrentScope().peek().getVariableTable();
+    }
+
+    @Test
+    public void test1() {
+        IntegerSymbol intSymbol = (IntegerSymbol) variableTable.get("ada");
+        assertTrue(intSymbol.getValue() == 3);
+    }
 }
