ba4245ba728b67e48d105333c783e8fd3d613563
Frederik <frederik@pretzmann.dk>
2024-05-25 01:16:43 +0200

    Replaced complex type check with nesting level check

--- Project/src/main/java/com/proj4/AST/visitors/CheckVisitors/AssignmentTypeChecker.java
+++ Project/src/main/java/com/proj4/AST/visitors/CheckVisitors/AssignmentTypeChecker.java
@@ -7,30 +7,43 @@ import com.proj4.exceptions.MismatchedTypeException;
 import com.proj4.AST.nodes.AST;
 import com.proj4.AST.nodes.Assignment;
 
-public class AssignmentTypeChecker implements NodeVisitor{
+public class AssignmentTypeChecker implements NodeVisitor {
 
-    public void visit(AST node){
+    public void visit(AST node) {
         Assignment assignment = (Assignment) node;
 
-        assignment.visitChild(new CheckDecider(), assignment.getSymbolExpression());    //check the symbol to overwrite
+        assignment.visitChild(new CheckDecider(), assignment.getSymbolExpression()); // check the symbol to overwrite
 
         String expectedType = TypeCheckVisitor.getInstance().getFoundType(); // This evaluates arrays correct
 
         String expectedComplexType = TypeCheckVisitor.getInstance().getFoundComplexType();
 
-        assignment.visitChild(new CheckDecider(), assignment.getValueExpression());    //check the value to overwrite with
+        Integer expectedNestingLevel = TypeCheckVisitor.getInstance().getNestingLevel();
+
+        assignment.visitChild(new CheckDecider(), assignment.getValueExpression()); // check the value to overwrite with
 
         String valueType = TypeCheckVisitor.getInstance().getFoundType();
 
         String valueComplexType = TypeCheckVisitor.getInstance().getFoundComplexType();
 
+        Integer valueNestingLevel = TypeCheckVisitor.getInstance().getNestingLevel();
+
+        // Nestinglevel
+        if (expectedNestingLevel != valueNestingLevel){
+            throw new MismatchedTypeException("Cannot assign value of complex type \"" + valueComplexType + "\"" + " with nestinglevel \"" + valueNestingLevel
+                    + "\" to variable of complex type \"" + expectedComplexType + "\" with nestinglevel \"" + expectedNestingLevel + "\"!");
+        }
         // Primitive types
-        if(!expectedType.equals(valueType) || valueType.equals("Null")){
-            throw new MismatchedTypeException("Cannot assign value of type \"" + TypeCheckVisitor.getInstance().getFoundType() + "\" to variable of type \"" + expectedType + "\"!");
+        if (!expectedType.equals(valueType) || valueType.equals("Null")) {
+            throw new MismatchedTypeException("Cannot assign value of type \"" + valueType
+                    + "\" to variable of type \"" + expectedType + "\"!");
         }
-            // Complex types
-        if (!expectedComplexType.equals(valueComplexType) || valueComplexType.equals("Null")) {
-            throw new MismatchedTypeException("Cannot assign value of complex type \"" + TypeCheckVisitor.getInstance().getFoundComplexType() + "\" to variable of type \"" + expectedComplexType + "\"!");
-        }
+
+        // Arrays should be the only case where we'd use complex type, so we can use nesting level instead of this
+        // Complex types
+        // if (!expectedComplexType.equals(valueComplexType) || valueComplexType.equals("Null")) {
+        //     throw new MismatchedTypeException("Cannot assign value of complex type \"" + valueComplexType
+        //             + "\" to variable of type \"" + expectedComplexType + "\"!");
+        // }
     }
 }
