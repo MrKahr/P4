6ca96f9787e9485691cccbd649723be6c651d887
Frederik <frederik@pretzmann.dk>
2024-05-25 01:19:13 +0200

    Debug stuff

--- Project/src/main/java/com/proj4/Main.java
+++ Project/src/main/java/com/proj4/Main.java
@@ -10,14 +10,16 @@ public class Main {
                                 "    Integer power;\r\n" + //
                                 "}\r\n" + //
                                 "\r\n" + //
+                                "Card c1 IS NEW Card{0; 0; 0;}\r\n" + //
+                                "\r\n" + //
                                 "Card[] deck;\r\n" + //
-                                "FOR(Integer i IS 0; i LESS OR EQUALS 10; i IS i+1){\r\n" + //
-                                "    deck[i] IS NEW Card{i; i+1; i+2;}\r\n" + //
+                                "FOR(Integer i IS 0; i LESS OR EQUALS 2; i IS i+1){\r\n" + //
+                                "    deck[i] IS NEW Card{1; 2; 3;};\r\n" + //
                                 "}"};
         }
         DBL interpreter = new DBL();
         interpreter.setVerbosity(true);
-        interpreter.setDebugMode(true);
+        interpreter.setDebugMode(false);
         interpreter.interpret(args[0]);
     }
 }
